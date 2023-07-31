864657d719be18265ae810300d5dbc5d1e479e15
SrinandanS <141049451+SrinandanS@users.noreply.github.com>
2023-07-31 21:40:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+async function Main() {
+  document.getElementById('start').style.display = 'none'
+  let file = await fetch('Frames.json')
+  let JSON = await file.json()
+  for(let x = 0; x < JSON.length; x++){
+    document.getElementById('text').innerHTML = JSON[x]
+    await new Promise(resolve => setTimeout(resolve, 30))
+    
+  }
+  
+}
+
+//let Char = `<span style="color: ${color};">@ </span>`;
